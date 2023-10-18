82fe72f9a66ba52ef9165f645faa949f2a26daf4
Peter Lauer <lauerpeter11@gmail.com>
2023-10-18 14:07:06 -0500

    my ideas for the project have changed, but the core of the site is the same. Added a user schema (server side)

---
+++ server/config/config.js
@@ -0,0 +1,22 @@
+
+
+const mongoose = require('mongoose');
+
+const db = mongoose.connection;
+
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/---------------'; 
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/--------------', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB database.');
+});
+
+module.exports = db;
---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+
+
+const userSchema = new Schema({
+  Uname: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
