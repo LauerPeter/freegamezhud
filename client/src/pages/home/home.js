8c9cec1bb7c984384e137982a5b51b76b8671b2a
Peter Lauer <lauerpeter11@gmail.com>
2023-10-31 17:54:42 -0500

    matched styling throughout the site, going to mess with the user component to view saved games

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,16 +1,17 @@
 
 
 import React from 'react';
+import './home.css'
 
 function Home() {
   return (
-    <div>
-    <h1>About The Website</h1>
-    <p>Welcome to the website, where you can access hundreds of free games!</p>
-    <p>The mission of this site is to provide a wide variety of entertaining games that you can enjoy without any cost.</p>
-    <p>Whether you're a casual gamer or a dedicated gaming enthusiast, you'll find something here to keep you entertained.</p>
-    <p>Feel free to browse the collection provided by https://www.freetogame.com/!</p>
-    <p>Save your favorite games, and have fun</p>
+  <div className="about-container">
+    <p className="about-title">About The Website</p>
+    <p className="about-paragraph">Welcome to the website, where you can access hundreds of free games!</p>
+    <p className="about-paragraph">The mission of this site is to provide a wide variety of entertaining games that you can enjoy without any cost.</p>
+    <p className="about-paragraph">Whether you're a casual gamer or a dedicated gaming enthusiast, you'll find something here to keep you entertained.</p>
+    <p className="about-paragraph">Feel free to browse the collection provided by https://www.freetogame.com/!</p>
+    <p className="about-paragraph">Save your favorite games, and have fun</p>
   </div>
   );
 }
