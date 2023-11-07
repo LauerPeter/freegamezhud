e60bfd896bc3f93493cb35fbd4ba5f1ee7c2ff9f
Peter Lauer <lauerpeter11@gmail.com>
2023-11-07 15:53:54 -0600

    user saved games display url title and image, will add the image clickable to send you back to gameDescrip aswell as styling

--- client/src/pages/user/user.js
+++ client/src/pages/user/user.js
@@ -1,22 +1,48 @@
 
 
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useAuthState } from '../../context/authContext';
 
 function UserProfile() {
-  const { Uname } = useAuthState(); 
+  const { Uname } = useAuthState();
   const savedGames = JSON.parse(localStorage.getItem('savedGames')) || [];
+  const [gameDetails, setGameDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchGameDetails = async () => {
+      const details = [];
+      for (const gameId of savedGames) {
+        try {
+          const response = await axios.get(`/api/game?id=${gameId}`);
+          details.push(response.data);
+        } catch (error) {
+          console.error('Axios Error:', error);
+        }
+      }
+      setGameDetails(details);
+    };
+
+    fetchGameDetails();
+  }, [savedGames]);
+
   return (
     <div className="container">
-    <h2>{Uname}'s Profile</h2>
-    <h3>Saved Games</h3>
-    <ul>
-      {savedGames.map((gameId) => (
-        <li key={gameId}>Game ID: {gameId}</li>
-      ))}
-    </ul>
-  </div>
-);
+      <h2>{Uname}'s Profile</h2>
+      <h3>Saved Games</h3>
+      <ul>
+        {gameDetails.map((game) => (
+          <li key={game.id}>
+            Game Title: {game.title}<br />
+            <img src={game.thumbnail} alt={game.title} /><br />
+            <a href={game.game_url} target="_blank" rel="noopener noreferrer">
+              {game.game_url}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default UserProfile;
